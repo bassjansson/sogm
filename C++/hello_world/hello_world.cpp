afab8a8a5a1a11e7364786c1448742aa110194ac
Bass Jansson <bassjansson@gmail.com>
2013-12-02 23:06:12 +0100

    Modified hello_world

--- C++/hello_world/hello_world.cpp
+++ C++/hello_world/hello_world.cpp
@@ -1,31 +1,10 @@
 // my first program in C++
 
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
 int main() {
-	float samplerate;
-	float freq;
-	float phase;
-	float samplelength;
-
-	cout << "Samplerate: ";
-	cin >> samplerate;
-	cout << "Frequency: ";
-	cin >> freq;
-	cout << "Phase: ";
-	cin >> phase;
-	cout << "Length in Samples: ";
-	cin >> samplelength;
-	cout << endl;
-
-	for(int i = 0; i < samplelength; i++) {
-		float amp = sin(2*M_PI * (freq / samplerate) * i + 2*M_PI * phase);
-		float iamp = int(amp * 10000) / 10000.;
-		cout << iamp << endl;
-	}
-	
+	cout << "Hello World!" << endl;
 	return 0;
 }
