ac5dae6847072dd387bfafaa75aa1ce994e953f2
Bass Jansson <bassjansson@gmail.com>
2014-01-10 10:02:33 +0100

    Improved sin_wave

--- C++/sin_wave/sin_wave.cpp
+++ C++/sin_wave/sin_wave.cpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <cmath>
 
+#define SAMPLERATE 48000
+
 using namespace std;
 
 int main() {
-	int window = 1024;
+	int freq = 480;
+	int window = SAMPLERATE / freq; // 1 periode
 
 	for(int i = 0; i < window; i++) {
 		cout << sin((double)i / window * 2*M_PI) << endl;
